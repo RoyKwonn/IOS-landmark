1e63911df9637c859927d213a5540e9635fe221c
Seokhwan-Kwon <sychar05@gmail.com>
2021-01-10 08:58:05 +0900

    IOS-landmark

---
+++ Seokhwan/Views/Badges/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Seokhwan
+//
+//  Created by Seokhwan Kwon on 2021/01/10.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
