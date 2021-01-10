ae71eaf734afc20ce1cb8f18570328cf28b7b319
Seokhwan-Kwon <sychar05@gmail.com>
2021-01-10 23:57:09 +0900

    IOS-landmark

--- Seokhwan/Model/ModelData.swift
+++ Seokhwan/Model/ModelData.swift
@@ -10,16 +10,15 @@ import Combine
 
 final class ModelData: ObservableObject {
     @Published var landmarks: [Landmark] = load("landmarkData.json")
+    var hikes: [Hike] = load("hikeData.json")
 }
-
-var landmarks: [Landmark] = load("landmarkData.json")
 
 func load<T: Decodable>(_ filename: String) -> T {
     let data: Data
 
     guard let file = Bundle.main.url(forResource: filename, withExtension: nil)
-    else {
-        fatalError("Couldn't find \(filename) in main bundle.")
+        else {
+            fatalError("Couldn't find \(filename) in main bundle.")
     }
 
     do {
@@ -35,3 +34,4 @@ func load<T: Decodable>(_ filename: String) -> T {
         fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
     }
 }
+
