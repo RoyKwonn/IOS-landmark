250cc5d98a31a12d1c12f791720f7704e244971d
Seokhwan-Kwon <sychar05@gmail.com>
2021-01-11 11:11:53 +0900

    IOS-landmark

---
+++ Seokhwan/Model/Profile.swift
@@ -0,0 +1,26 @@
+//
+//  Profile.swift
+//  Seokhwan
+//
+//  Created by Seokhwan Kwon on 2021/01/11.
+//
+
+import Foundation
+
+struct Profile {
+    var username: String
+    var prefersNotifications = true
+    var seasonalPhoto = Season.winter
+    var goalDate = Date()
+
+    static let `default` = Profile(username: "@seok.kan")
+
+    enum Season: String, CaseIterable, Identifiable {
+        case spring = "🌷"
+        case summer = "🌞"
+        case autumn = "🍂"
+        case winter = "☃️"
+
+        var id: String { self.rawValue }
+    }
+}
