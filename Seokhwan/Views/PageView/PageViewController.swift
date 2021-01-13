99e97366592ac20f042ea5d7f0b6fa8d6fa9112e
Seokhwan-Kwon <sychar05@gmail.com>
2021-01-14 02:32:42 +0900

    IOS-landmark

---
+++ Seokhwan/Views/PageView/PageViewController.swift
@@ -0,0 +1,26 @@
+//
+//  PageViewController.swift
+//  Seokhwan
+//
+//  Created by Seokhwan Kwon on 2021/01/14.
+//
+
+import SwiftUI
+import UIKit
+
+struct PageViewController<Page: View>: UIViewControllerRepresentable {
+    var pages: [Page]
+
+    func makeUIViewController(context: Context) -> UIPageViewController {
+        let pageViewController = UIPageViewController(
+            transitionStyle: .scroll,
+            navigationOrientation: .horizontal)
+
+        return pageViewController
+    }
+
+    func updateUIViewController(_ pageViewController: UIPageViewController, context: Context) {
+        pageViewController.setViewControllers(
+            [UIHostingController(rootView: pages[0])], direction: .forward, animated: true)
+    }
+}
